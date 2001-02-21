4d4b953317e42ccd052d54386256e6a648aa3364
Karsten Keil <keil@temic-ech.spacenet.de>
2001-02-21 19:22:35 +0000

    begin capi

---
+++ drivers/isdn/hardware/mISDN/asn1_basic_service.h
@@ -0,0 +1,9 @@
+/* $Id$
+ *
+ */
+
+// ======================================================================
+// Basic Service Elements EN 300 196-1 D.6
+
+int ParseBasicService(struct asn1_parm *pc, u_char *p, u_char *end, int *basicService);
+
