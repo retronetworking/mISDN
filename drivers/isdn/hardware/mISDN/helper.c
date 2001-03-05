39435e1d277d0a192df36a0448d1ed009f0e21db
Karsten Keil <keil@temic-ech.spacenet.de>
2001-03-05 23:13:19 +0000

    init blog

--- drivers/isdn/hardware/mISDN/helper.c
+++ drivers/isdn/hardware/mISDN/helper.c
@@ -96,14 +96,23 @@ free_dchannel(dchannel_t *dch) {
 
 int
 init_bchannel(bchannel_t *bch) {
+	if (!(bch->blog = kmalloc(MAX_BLOG_SPACE, GFP_ATOMIC))) {
+		printk(KERN_WARNING
+			"HiSax: No memory for blog\n");
+		return(-ENOMEM);
+	}
 	if (!(bch->rx_buf = kmalloc(MAX_DATA_MEM, GFP_ATOMIC))) {
 		printk(KERN_WARNING
 			"HiSax: No memory for bchannel rx_buf\n");
+		kfree(bch->blog);
+		bch->blog = NULL;
 		return (-ENOMEM);
 	}
 	if (!(bch->tx_buf = kmalloc(MAX_DATA_MEM, GFP_ATOMIC))) {
 		printk(KERN_WARNING
 			"HiSax: No memory for bchannel tx_buf\n");
+		kfree(bch->blog);
+		bch->blog = NULL;
 		kfree(bch->rx_buf);
 		bch->rx_buf = NULL;
 		return (-ENOMEM);
@@ -125,6 +134,10 @@ free_bchannel(bchannel_t *bch) {
 	if (bch->tqueue.sync)
 		printk(KERN_ERR"free_bchannel tqueue.sync\n");
 	discard_queue(&bch->rqueue);
+	if (bch->blog) {
+		kfree(bch->blog);
+		bch->blog = NULL;
+	}
 	if (bch->rx_buf) {
 		kfree(bch->rx_buf);
 		bch->rx_buf = NULL;
