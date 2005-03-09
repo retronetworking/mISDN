5e7f03944c88fd947cc74bf10c2e9c7fc01ebe76
Karsten Keil <keil@temic-ech.spacenet.de>
2005-03-09 03:04:07 +0000

    fix for changes in driver_register for 2.6.10 and higher versions

--- drivers/isdn/hardware/mISDN/w6692.c
+++ drivers/isdn/hardware/mISDN/w6692.c
@@ -1503,7 +1503,7 @@ static int __devinit w6692_probe(struct pci_dev *pdev, const struct pci_device_i
 	}
 
 	printk(KERN_INFO "mISDN_w6692: found adapter %s at %s\n",
-	       (char *) ent->driver_data, pdev->slot_name);
+	       (char *) ent->driver_data, pci_name(pdev));
 
 	card->addr = pci_resource_start(pdev, 1);
 	card->irq = pdev->irq;
@@ -1580,11 +1580,13 @@ static int __init w6692_init(void)
 	if (err < 0)
 		goto out;
 
+#ifdef OLD_PCI_REGISTER_DRIVER
 	if (err == 0) {
 		err = -ENODEV;
 		pci_unregister_driver(&w6692_driver);
 		goto out;
 	}
+#endif
 	return 0;
 
  out:
