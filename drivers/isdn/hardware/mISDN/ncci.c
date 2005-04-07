8101522abc97dbe8ddd254a240c2ae902e71e322
Karsten Keil <keil@temic-ech.spacenet.de>
2005-04-07 08:57:50 +0000

    debug wrong conf frames

--- drivers/isdn/hardware/mISDN/ncci.c
+++ drivers/isdn/hardware/mISDN/ncci.c
@@ -1043,6 +1043,9 @@ ncciDataConf(Ncci_t *ncci, int pr, struct sk_buff *skb)
 	}
 	if (i == ncci->window) {
 		int_error();
+		printk(KERN_DEBUG "%s: dinfo(%x)\n", __FUNCTION__, mISDN_HEAD_DINFO(skb));
+		for (i = 0; i < ncci->window; i++)
+			printk(KERN_DEBUG "%s: PktId[%d] %x\n", __FUNCTION__, i, ncci->xmit_skb_handles[i].PktId);
 		return(-EINVAL);
 	}
 	ncci->xmit_skb_handles[i].PktId = 0;
