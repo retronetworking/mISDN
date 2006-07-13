1c93a4f475194e2ccb0c6ef633828d60cdf17a90
Chrisian Richter <christian.richter@beronet.com>
2006-07-13 09:11:32 +0000

    for kernels >2.6.10 we need to release the port in the RELEASE message, this is not everytime true, but it doesn't harm anything to do it.. only the leaving mSDNstackd processes. This really needs rework

--- drivers/isdn/hardware/mISDN/hfc_multi.c
+++ drivers/isdn/hardware/mISDN/hfc_multi.c
@@ -3206,7 +3206,7 @@ bugtest
 		case MGR_RELEASE | INDICATION:
 		if (debug & DEBUG_HFCMULTI_MGR)
 			printk(KERN_DEBUG "%s: MGR_RELEASE = remove port from mISDN\n", __FUNCTION__);
-#if 0
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,10) 
 		if (test_bit(FLG_DCHANNEL, &chan->Flags))
 			release_port(hc, hc->chan[ch].port); /* hc is free */
 #endif
