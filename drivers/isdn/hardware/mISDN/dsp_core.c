c34e06a2dde9442a1cd12ff502f9d3e74b8a2ef7
Chrisian Richter <christian.richter@beronet.com>
2007-03-23 17:23:03 +0000

    workaround for Kernel Oops.. fixing real root soon :)

--- drivers/isdn/hardware/mISDN/dsp_core.c
+++ drivers/isdn/hardware/mISDN/dsp_core.c
@@ -476,6 +476,11 @@ dsp_from_up(mISDNinstance_t *inst, struct sk_buff *skb)
 				}
 
 			} else {
+				if (dsp->features.pcm_id>=0) {
+					printk("Not sending Data to CMX -- > returning because of HW bridge\n");
+					dev_kfree_skb(skb);
+					break;
+				}
 				/* send data to tx-buffer (if no tone is played) */
 				spin_lock_irqsave(&dsp_obj.lock, flags);
 				if (!dsp->tone.tone) {
